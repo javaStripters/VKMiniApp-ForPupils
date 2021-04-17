6da4682233f1d6cf512020d4342a5c7026143630
cntgfy111 <55919118+cntgfy111@users.noreply.github.com>
2021-04-17 17:30:48 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/sh
 
-export NODE_ENV="production"
-
 cd front || exit
-npm i --force && npm run build
+npm i --force 
+npm run build
 
 cd ../back || exit
-npm i && npm run start
+npm i 
+npm run start
